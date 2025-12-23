289efdf29979a80f24d86bd89432d506835a20cb
Daniel Carolino <99697178+DanielCarolino89@users.noreply.github.com>
2025-12-22 22:42:42 -0300

    Delete assets directory

--- assets/JS/scripts.js
+++
@@ -1,5 +0,0 @@
-// Função para pré-visualizar a foto selecionada
-function previewFoto(event) {
-    const img = document.getElementById('preview');
-    img.src = URL.createObjectURL(event.target.files[0]);
-}
